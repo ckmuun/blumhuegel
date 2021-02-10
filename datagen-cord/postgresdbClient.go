f7fafd8b6add7d96b11a3c0d403d5598495d6bda
ckmuun <cornelius.koller@online.de>
2021-02-10 07:50:07 +0100

    rm postgresdbClient.go, we'll stick with in-memory collections for now.

--- datagen-cord/postgresdbClient.go
+++
@@ -1,9 +0,0 @@
-package main
-
-/*
-	TODO decide whether we actually need an external datastore or whether we just keep the data in mem
-*/
-
-func persistSymbolsList() {
-	// TODO implement me...
-}
