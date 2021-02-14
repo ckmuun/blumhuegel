524aab060a347e01e981c76f267ac37799351c6d
ckmuun <cornelius.koller@online.de>
2021-02-14 20:22:02 +0100

    rm unnecessary files

--- datagen-cord/setup-mk-postgres.sh
+++
@@ -1,4 +0,0 @@
-echo "setting up postgres db in local minikube using helm"
-
-# TODO check whether we actually need this. (e.g. if an in-memory collection will do)
-# and implement or delete accordingly
